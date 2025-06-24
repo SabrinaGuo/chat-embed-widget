99f72a9d7d9c5f9a1d55dfb40eca62db3764e71c
Aaron Chiou <aaron.chiou@nextlink.com.tw>
2025-06-24 09:56:32 +0800

    style: adjust formatting and structure of chatbot initialization code

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-    const satisfactionHTML = `
+const satisfactionHTML = `
 <div class="bot-feedback-block">
     <div class="text-style">Was this answer helpful to you?</div>
     <div class="feedback-buttons">
@@ -9,7 +9,7 @@
 </div>
 `;
 
-    const enterQuestionnaireHTML = `
+const enterQuestionnaireHTML = `
 <div class="bot-feedback-block">
     <div class="text-style">Sorry we couldn’t assist you this time. Please leave your information via our  
     <a class="link-style" href="https://aicom.nextlink.cloud/en/contact-us/" target="_blank" rel="noopener noreferrer">Contact Us</a> 
@@ -18,249 +18,254 @@
 </div>
 `;
 
-    const defaultCallSatisfactionStr = [
-    "謝謝你",
-    "太感謝了！",
-    "了解了",
-    "沒問題了",
-    "我知道該怎麼做了",
-    "Thanks, Thank you",
-    "Much appreciated!",
-    "Got it, I see",
-    "That's all, No more questions",
-    "I know what to do now",
-    ];
-    const enterQuestionnaireStr = [
-    "你沒幫上忙",
-    "這不是我要的答案",
-    "我想跟真人聊",
-    "That didn’t help",
-    "That’s not what I asked",
-    "I want to talk to a human",
-    ];
+const defaultCallSatisfactionStr = [
+	"謝謝你",
+	"太感謝了！",
+	"了解了",
+	"沒問題了",
+	"我知道該怎麼做了",
+	"Thanks, Thank you",
+	"Much appreciated!",
+	"Got it, I see",
+	"That's all, No more questions",
+	"I know what to do now",
+];
+const enterQuestionnaireStr = [
+	"你沒幫上忙",
+	"這不是我要的答案",
+	"我想跟真人聊",
+	"That didn’t help",
+	"That’s not what I asked",
+	"I want to talk to a human",
+];
 
-    let connecting = false;
-    let isComposing = false;
-    let isFeedbackInputComposing = false;
+let connecting = false;
+let isComposing = false;
+let isFeedbackInputComposing = false;
 
-    const userInput = document.getElementById("userInput");
-    const sendBtn = document.querySelector(".send-btn");
+const userInput = document.getElementById("userInput");
+const sendBtn = document.querySelector(".send-btn");
 
-    function init() {
-    // messages.innerHTML = '';
-    createBotMsg("Hi there! I'm Poco. How can I assist you today?");
-    }
-    init();
+function init() {
+	// messages.innerHTML = '';
+	createBotMsg("Hi there! I'm Poco. How can I assist you today?");
+}
+init();
 
-    function linkify(text) {
-        console.log(text)
-    const urlRegex =
-        /(https?:\/\/[^\s\u3002\uff0c\u3001\uFF1F\uFF01\uFF1B\uFF1A\u300C\u300D\u300E\u300F\u201C\u201D\u300A\u300B\u3010\u3011\uFF08\uFF09]+)/g;
-    return text.replace(urlRegex, (url) => {
-        return `<a class="link-style" href="${url}" target="_blank" rel="noopener noreferrer">${url}</a>`;
-    });
-    }
+function linkify(text) {
+	console.log(text);
+	const urlRegex =
+		/(https?:\/\/[^\s\u3002\uff0c\u3001\uFF1F\uFF01\uFF1B\uFF1A\u300C\u300D\u300E\u300F\u201C\u201D\u300A\u300B\u3010\u3011\uFF08\uFF09]+)/g;
+	return text.replace(urlRegex, (url) => {
+		return `<a class="link-style" href="${url}" target="_blank" rel="noopener noreferrer">${url}</a>`;
+	});
+}
 
-    function formatTime() {
-    const now = new Date();
-    const h = now.getHours().toString().padStart(2, "0");
-    const m = now.getMinutes().toString().padStart(2, "0");
-    return `${h}:${m}`;
-    }
+function formatTime() {
+	const now = new Date();
+	const h = now.getHours().toString().padStart(2, "0");
+	const m = now.getMinutes().toString().padStart(2, "0");
+	return `${h}:${m}`;
+}
 
-    function createBotLoading() {
-    const botBox = document.createElement("div");
-    botBox.className = "messages-bot messages-box loading-box";
+function createBotLoading() {
+	const botBox = document.createElement("div");
+	botBox.className = "messages-bot messages-box loading-box";
 
-    const botBoxContent = document.createElement("div");
-    botBoxContent.className = "messages-content";
+	const botBoxContent = document.createElement("div");
+	botBoxContent.className = "messages-content";
 
-    const botIcon = document.createElement('div')
-    botIcon.className = 'messages-bot-icon'
-    botIcon.style.background = 'linear-gradient(90deg, #bf51dc 0%, #5e55fa 100%)'
+	const botIcon = document.createElement("div");
+	botIcon.className = "messages-bot-icon";
+	botIcon.style.background = "linear-gradient(90deg, #bf51dc 0%, #5e55fa 100%)";
 
-    const botImg = document.createElement("img");
-    botImg.src = "./img/icon-bot.svg";
+	const botImg = document.createElement("img");
+	botImg.src = "./img/icon-bot.svg";
 
-    const loading = document.createElement("div");
-    loading.className = "spinner";
-    loading.innerHTML = `
+	const loading = document.createElement("div");
+	loading.className = "spinner";
+	loading.innerHTML = `
         <div class="bounce1"></div>
         <div class="bounce2"></div>
         <div class="bounce3"></div>
     `;
 
-    botIcon.appendChild(botImg)
-    botBox.appendChild(botIcon);
-    botBox.appendChild(botBoxContent);
-    botBoxContent.appendChild(loading);
-    messages.appendChild(botBox);
+	botIcon.appendChild(botImg);
+	botBox.appendChild(botIcon);
+	botBox.appendChild(botBoxContent);
+	botBoxContent.appendChild(loading);
 
-    return botBox;
-    }
+	messages.appendChild(botBox);
 
-    function createUserMsg(str) {
-    const userBox = document.createElement("div");
-    userBox.className = "messages-user messages-box";
-    const userTime = document.createElement("span");
-    userTime.textContent = formatTime();
-    const userBoxContent = document.createElement("div");
-    userBoxContent.className = "messages-content";
-    const userP = document.createElement("div");
-    userP.className = "text-style";
-    userP.innerHTML = str;
+	return botBox;
+}
 
-    userBox.appendChild(userTime);
-    userBox.appendChild(userBoxContent);
-    userBoxContent.appendChild(userP);
-    messages.appendChild(userBox);
-    messages.scrollTop = messages.scrollHeight;
-    }
+function createUserMsg(str) {
+	const userBox = document.createElement("div");
+	userBox.className = "messages-user messages-box";
 
-    function createBotMsg(str) {
-    const botBox = document.createElement("div");
-    botBox.className = "messages-bot messages-box";
+	const userTime = document.createElement("span");
+	userTime.textContent = formatTime();
 
-    const botBoxContent = document.createElement("div");
-    botBoxContent.className = "messages-content";
+	const userBoxContent = document.createElement("div");
+	userBoxContent.className = "messages-content";
 
-    const botIcon = document.createElement('div')
-    botIcon.className = 'messages-bot-icon'
-    botIcon.style.background = 'linear-gradient(90deg, #bf51dc 0%, #5e55fa 100%)'
+	const userP = document.createElement("div");
+	userP.className = "text-style";
+	userP.innerHTML = str;
 
-    const botImg = document.createElement("img");
-    botImg.src = "./img/icon-bot.svg";
+	userBox.appendChild(userTime);
+	userBox.appendChild(userBoxContent);
+	userBoxContent.appendChild(userP);
 
-    const botTime = document.createElement("span");
-    botTime.textContent = formatTime();
-    console.log(1)
-    console.log(defaultCallSatisfactionStr)
-    console.log(str)
-    if (defaultCallSatisfactionStr.includes(str)) {
-        console.log(2)
-        botBoxContent.innerHTML = satisfactionHTML;
-    } else if (enterQuestionnaireStr.includes(str)) {
-        botBoxContent.innerHTML = enterQuestionnaireHTML;
-    } else {
-        const botP = document.createElement("div");
-        botP.className = "text-style";
-        botP.innerHTML = linkify(str);
-        botBoxContent.appendChild(botP);
-    }
+	messages.appendChild(userBox);
+	messages.scrollTop = messages.scrollHeight;
+}
 
-    botIcon.appendChild(botImg)
-    botBox.appendChild(botIcon);
-    botBox.appendChild(botBoxContent);
-    botBox.appendChild(botTime);
-    messages.appendChild(botBox);
+function createBotMsg(str) {
+	const botBox = document.createElement("div");
+	botBox.className = "messages-bot messages-box";
 
-    messages.scrollTop = messages.scrollHeight;
-    }
+	const botBoxContent = document.createElement("div");
+	botBoxContent.className = "messages-content";
 
-    function getMessageAndSend() {
-    const msg = userInput.value.trim();
-    if (!msg) return;
+	const botIcon = document.createElement("div");
+	botIcon.className = "messages-bot-icon";
+	botIcon.style.background = "linear-gradient(90deg, #bf51dc 0%, #5e55fa 100%)";
 
-    userInput.value = ""; // 同步清空
-    sendMessage(msg); // 把訊息傳進去
-    }
+	const botImg = document.createElement("img");
+	botImg.src = "./img/icon-bot.svg";
 
-    async function sendMessage(msg) {
-    const messages = document.getElementById("messages");
-    if (!msg) return;
-    createUserMsg(msg);
-    const loadingEl = createBotLoading();
+	const botTime = document.createElement("span");
+	botTime.textContent = formatTime();
+	console.log(1);
+	console.log(defaultCallSatisfactionStr);
+	console.log(str);
+	if (defaultCallSatisfactionStr.includes(str)) {
+		console.log(2);
+		botBoxContent.innerHTML = satisfactionHTML;
+	} else if (enterQuestionnaireStr.includes(str)) {
+		botBoxContent.innerHTML = enterQuestionnaireHTML;
+	} else {
+		const botP = document.createElement("div");
+		botP.className = "text-style";
+		botP.innerHTML = linkify(str);
+		botBoxContent.appendChild(botP);
+	}
 
-    if (defaultCallSatisfactionStr.includes(msg)) {
-        loadingEl.remove();
-        createBotMsg(satisfactionHTML);
-        return;
-    }
+	botIcon.appendChild(botImg);
+	botBox.appendChild(botIcon);
+	botBox.appendChild(botBoxContent);
+	botBox.appendChild(botTime);
+	messages.appendChild(botBox);
 
-    if (!connecting) {
-        connecting = true;
-        sendBtn.classList.add("disabled");
-        try {
-        const response = await fetch(
-            "https://billing-ai-temp-505738238648.asia-east1.run.app/api/v1/demo/stream_ask_question",
-            {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                question_str: msg,
-                account_restrictions: [],
-            }),
-            }
-        );
-        const replyText = await response.text();
-        loadingEl.remove();
-        createBotMsg(replyText);
-        connecting = false;
-        sendBtn.classList.remove("disabled");
-        } catch (error) {
-        if (loadingEl) loadingEl.remove();
-        createBotMsg(error.messages);
-        connecting = false;
-        sendBtn.classList.remove("disabled");
-        }
-    }
-    }
+	messages.scrollTop = messages.scrollHeight;
+}
 
-    function showText(str) {
-    createUserMsg(str);
-    setTimeout(() => {
-        if (str === "Yes") {
-        createBotMsg(
-            "Thank you for your feedback! I'm glad I could help. If you have any other questions, feel free to chat with me anythime!"
-        );
-        return;
-        }
-        createBotMsg("That didn’t help");
-    }, 600);
-    }
+function getMessageAndSend() {
+	const msg = userInput.value.trim();
+	if (!msg) return;
 
-    sendBtn.addEventListener("click", () => {
-    getMessageAndSend();
-    });
-    // 判斷是否正在輸入中文（選字階段）
-    userInput.addEventListener("compositionstart", () => {
-    isComposing = true;
-    });
+	userInput.value = ""; // 同步清空
+	sendMessage(msg); // 把訊息傳進去
+}
 
-    document.addEventListener("keydown", (e) => {
-    const target = e.target;
-    if (
-        target.matches("#feedback-input") &&
-        e.key === "Enter" &&
-        !e.shiftKey &&
-        !isFeedbackInputComposing
-    ) {
-        const message = target.value?.trim();
-        if (message) {
-        // createUserMsg(message);
-        e.preventDefault();
-        createBotMsg(
-            "Thanks again for your valuable feedback! I'll keep improving to deliver a better experience in the future."
-        );
-        // target.value = "";
-        }
-    }
-    });
+async function sendMessage(msg) {
+	const messages = document.getElementById("messages");
+	if (!msg) return;
+	createUserMsg(msg);
+	const loadingEl = createBotLoading();
 
-    userInput.addEventListener("compositionend", () => {
-    isComposing = false;
-    });
+	if (defaultCallSatisfactionStr.includes(msg)) {
+		loadingEl.remove();
+		createBotMsg(satisfactionHTML);
+		return;
+	}
 
-    document.getElementById("userInput").addEventListener("keydown", (e) => {
-    if (e.key === "Enter" && !isComposing) {
-        e.preventDefault(); // 阻止換行
-        getMessageAndSend();
-    }
-    });
+	if (!connecting) {
+		connecting = true;
+		sendBtn.classList.add("disabled");
+		try {
+			const response = await fetch(
+				"https://billing-ai-temp-505738238648.asia-east1.run.app/api/v1/demo/stream_ask_question",
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						question_str: msg,
+						account_restrictions: [],
+					}),
+				}
+			);
+			const replyText = await response.text();
+			loadingEl.remove();
+			createBotMsg(replyText);
+			connecting = false;
+			sendBtn.classList.remove("disabled");
+		} catch (error) {
+			if (loadingEl) loadingEl.remove();
+			createBotMsg(error.messages);
+			connecting = false;
+			sendBtn.classList.remove("disabled");
+		}
+	}
+}
 
-    document.querySelector(".close-btn").addEventListener("click", () => {
-    // 通知父視窗隱藏 iframe
-    window.parent.postMessage("closeChatIframe", "*");
-    });
+function showText(str) {
+	createUserMsg(str);
+	setTimeout(() => {
+		if (str === "Yes") {
+			createBotMsg(
+				"Thank you for your feedback! I'm glad I could help. If you have any other questions, feel free to chat with me anythime!"
+			);
+			return;
+		}
+		createBotMsg("That didn’t help");
+	}, 600);
+}
+
+sendBtn.addEventListener("click", () => {
+	getMessageAndSend();
+});
+// 判斷是否正在輸入中文（選字階段）
+userInput.addEventListener("compositionstart", () => {
+	isComposing = true;
+});
+
+document.addEventListener("keydown", (e) => {
+	const target = e.target;
+	if (
+		target.matches("#feedback-input") &&
+		e.key === "Enter" &&
+		!e.shiftKey &&
+		!isFeedbackInputComposing
+	) {
+		const message = target.value?.trim();
+		if (message) {
+			// createUserMsg(message);
+			e.preventDefault();
+			createBotMsg(
+				"Thanks again for your valuable feedback! I'll keep improving to deliver a better experience in the future."
+			);
+			// target.value = "";
+		}
+	}
+});
+
+userInput.addEventListener("compositionend", () => {
+	isComposing = false;
+});
+
+document.getElementById("userInput").addEventListener("keydown", (e) => {
+	if (e.key === "Enter" && !isComposing) {
+		e.preventDefault(); // 阻止換行
+		getMessageAndSend();
+	}
+});
+
+document.querySelector(".close-btn").addEventListener("click", () => {
+	// 通知父視窗隱藏 iframe
+	window.parent.postMessage("closeChatIframe", "*");
+});
--- js/init.js
+++ js/init.js
@@ -1,98 +1,98 @@
-(function() {
-    const BOT_DOMAIN = 'https://sabrinaguo.github.io'
-    const BOT_SRC = `${BOT_DOMAIN}/chat-embed-widget`
-    // const BOT_SRC = `./index.html`
-    const BOT_IMG = `${BOT_SRC}/img/icon-bot.svg`
-    const BOT_ARROW = `${BOT_SRC}/img/icon-arrow.svg`
+(function () {
+	const BOT_DOMAIN = "https://sabrinaguo.github.io";
+	const BOT_SRC = `${BOT_DOMAIN}/chat-embed-widget`;
+	// const BOT_SRC = `./index.html`
+	const BOT_IMG = `${BOT_SRC}/img/icon-bot.svg`;
+	const BOT_ARROW = `${BOT_SRC}/img/icon-arrow.svg`;
 
-    let isOpenBot = false
+	let isOpenBot = false;
 
-    const WRAPPER = document.createElement('div')
-    WRAPPER.style.position = 'fixed'
-    WRAPPER.style.right = '10px'
-    WRAPPER.style.bottom = '20px'
-    WRAPPER.style.cursor = 'pointer'
-    WRAPPER.style.zIndex = '9999'
-    WRAPPER.style.overflow = 'hidden'
+	const WRAPPER = document.createElement("div");
+	WRAPPER.style.position = "fixed";
+	WRAPPER.style.right = "10px";
+	WRAPPER.style.bottom = "20px";
+	WRAPPER.style.zIndex = "9999";
+	WRAPPER.style.overflow = "hidden";
 
-    const BOT_BTN = document.createElement('div')
-    BOT_BTN.id = 'LM-chatbot-btn'
-    BOT_BTN.style.width = '48px'
-    BOT_BTN.style.height = '48px'
-    BOT_BTN.style.border = 'none'
-    BOT_BTN.style.borderRadius = '50%'
-    BOT_BTN.style.background = "linear-gradient(90deg, #BF51DC 0%, #5E55FA 100%)"
-    BOT_BTN.style.position = 'absolute'
-    BOT_BTN.style.right = '10px'
-    BOT_BTN.style.bottom = '0'
-    BOT_BTN.style.display = 'flex'
-    BOT_BTN.style.justifyContent = 'center'
-    BOT_BTN.style.alignItems = 'center'
+	const BOT_BTN = document.createElement("div");
+	BOT_BTN.id = "LM-chatbot-btn";
+	BOT_BTN.style.width = "48px";
+	BOT_BTN.style.height = "48px";
+	BOT_BTN.style.border = "none";
+	BOT_BTN.style.borderRadius = "50%";
+	BOT_BTN.style.background = "linear-gradient(90deg, #BF51DC 0%, #5E55FA 100%)";
+	BOT_BTN.style.position = "absolute";
+	BOT_BTN.style.right = "10px";
+	BOT_BTN.style.bottom = "0";
+	BOT_BTN.style.display = "flex";
+	BOT_BTN.style.justifyContent = "center";
+	BOT_BTN.style.alignItems = "center";
+	BOT_BTN.style.cursor = "pointer";
 
-    const BTN_ICON = document.createElement('div')
-    BTN_ICON.style.backgroundRepeat = 'no-repeat'
-    BTN_ICON.style.backgroundSize = '100% 100%'
-    BTN_ICON.style.marginBottom = '5px'
-    BTN_ICON.style.userSelect = 'none'
+	const BTN_ICON = document.createElement("div");
+	BTN_ICON.style.backgroundRepeat = "no-repeat";
+	BTN_ICON.style.backgroundSize = "100% 100%";
+	BTN_ICON.style.marginBottom = "5px";
+	BTN_ICON.style.userSelect = "none";
 
-    const IFRAME = document.createElement('iframe')
-    IFRAME.src = BOT_SRC
-    IFRAME.style.width = 'calc(100% - 20px)'
-    IFRAME.style.height = '520px'
-    IFRAME.style.border = 'none'
-    IFRAME.style.userSelect = 'none'
-    IFRAME.style.borderRadius = '20px'
-    IFRAME.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)'
-    IFRAME.style.margin = '10px auto 0'
+	const IFRAME = document.createElement("iframe");
+	IFRAME.src = BOT_SRC;
+	IFRAME.style.width = "calc(100% - 20px)";
+	IFRAME.style.height = "520px";
+	IFRAME.style.border = "none";
+	IFRAME.style.userSelect = "none";
+	IFRAME.style.borderRadius = "20px";
+	IFRAME.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.1)";
+	IFRAME.style.margin = "10px auto 0";
 
-    setCloseStyle()
+	setCloseStyle();
 
-    WRAPPER.appendChild(IFRAME)
-    WRAPPER.appendChild(BOT_BTN)
-    BOT_BTN.appendChild(BTN_ICON)
-    document.body.appendChild(WRAPPER)
+	WRAPPER.appendChild(IFRAME);
+	WRAPPER.appendChild(BOT_BTN);
+	BOT_BTN.appendChild(BTN_ICON);
+	document.body.appendChild(WRAPPER);
 
-    BOT_BTN.addEventListener('click', handleOpenBot)
+	BOT_BTN.addEventListener("click", handleOpenBot);
 
-    window.addEventListener('message', (event) => {
-        // 可以檢查來源安全性（建議）
-        // if (event.origin !== 'https://xxx.com') return;
-        if (event.data === 'closeChatIframe') {
-            isOpenBot = false
-            setCloseStyle()
-        }
-    });
+	window.addEventListener("message", (event) => {
+		// 可以檢查來源安全性（建議）
+		// if (event.origin !== 'https://xxx.com') return;
+		if (event.data === "closeChatIframe") {
+			isOpenBot = false;
+			setCloseStyle();
+		}
+	});
 
-    function handleOpenBot () {
-        isOpenBot = !isOpenBot
-        if (!isOpenBot) {
-            setCloseStyle()
-        } else {
-            setOpenStyle()
-        }
-    }
+	function handleOpenBot() {
+		isOpenBot = !isOpenBot;
+		if (!isOpenBot) {
+			setCloseStyle();
+		} else {
+			setOpenStyle();
+		}
+	}
 
-    function setCloseStyle () {
-        WRAPPER.style.width = '58px'
-        WRAPPER.style.height = '48px'
+	function setCloseStyle() {
+		WRAPPER.style.width = "58px";
+		WRAPPER.style.height = "48px";
 
-        IFRAME.style.display = 'none'
+		IFRAME.style.display = "none";
 
-        BTN_ICON.style.width = '34px'
-        BTN_ICON.style.height = '32px'
-        BTN_ICON.style.backgroundImage = `url(${BOT_IMG})`
-        BTN_ICON.style.marginBottom = '5px'
-    }
+		BTN_ICON.style.width = "34px";
+		BTN_ICON.style.height = "32px";
+		BTN_ICON.style.backgroundImage = `url(${BOT_IMG})`;
+		BTN_ICON.style.marginBottom = "5px";
+	}
 
-    function setOpenStyle () {
-        WRAPPER.style.width = '380px'
-        WRAPPER.style.height = '595px'
+	function setOpenStyle() {
+		WRAPPER.style.width = "380px";
+		WRAPPER.style.height = "595px";
 
-        IFRAME.style.display = 'block'
+		IFRAME.style.display = "block";
 
-        BTN_ICON.style.width = '26px'
-        BTN_ICON.style.height = '26px'
-        BTN_ICON.style.marginBottom = '0px'
-        BTN_ICON.style.backgroundImage = `url(${BOT_ARROW})`
-    }
-})()
+		BTN_ICON.style.width = "26px";
+		BTN_ICON.style.height = "26px";
+		BTN_ICON.style.marginBottom = "0px";
+		BTN_ICON.style.backgroundImage = `url(${BOT_ARROW})`;
+	}
+})();
